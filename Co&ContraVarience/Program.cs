1e9b26660717a0997c32ec4e6c4b527f6acb4b40
prachik167 <128143076+prachik167@users.noreply.github.com>
2025-02-04 14:12:59 +0530

    Add files via upload

---
+++ Co&ContraVarience/Program.cs
@@ -0,0 +1,24 @@
+﻿using Co_ContraVarience;
+
+delegate Vehicle delveh();
+delegate void delcontra(Car car);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Vehicle vhl = new Vehicle();
+        delveh delv = new delveh(vhl.CreateVehicle);
+
+        Car cr = new Car();
+    
+        delv = new delveh(cr.CreateCar);
+
+        delcontra delcont = new delcontra(vhl.Display);
+        delcont.Invoke(cr);
+
+        delcont += cr.DisplayCar;
+
+    }
+}
---
+++ Co&ContraVarience/Vehicle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Co_ContraVarience
+{
+    internal class Vehicle
+    {
+        public Vehicle CreateVehicle()
+        {
+            Console.WriteLine("Vehicle created");
+            return new Vehicle();
+        }
+        public void Display(Vehicle vehobj)
+        {
+            Console.WriteLine("Vehicles class Method" + vehobj);
+        }
+
+    }
+    class Car : Vehicle
+    {
+        public Car CreateCar()
+        {
+            Console.WriteLine("Car Created");
+            return new Car();
+        }
+        public void DisplayCar(Car carobj)
+        {
+            Console.WriteLine("Car class Method" + carobj);
+        }
+    }
+}
+    
