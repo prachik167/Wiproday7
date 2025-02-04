10e1385975b82807fe3cf35714ac179a4a6c2029
prachik167 <128143076+prachik167@users.noreply.github.com>
2025-02-04 14:10:17 +0530

    Add files via upload

---
+++ MulticastDelegate/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MulticastDelegate
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MathopDel d = new MathopDel(MathOpe.Add);
+            d.Invoke(10, 20);
+            d += MathOpe.Sub;
+            d += MathOpe.Mul;
+            d += MathOpe.Div;
+            d(20, 10);
+        }
+    }
+}
