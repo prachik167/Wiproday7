e8795817869dd3cb60880064ed78869a09ff3e27
prachik167 <128143076+prachik167@users.noreply.github.com>
2025-02-04 14:02:08 +0530

    Add files via upload

---
+++ Day7/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    public delegate void delSample(string s);
+
+    private static void Main(string[] args)
+    {
+        delSample del = new delSample(Display);
+        del("Hello");
+    }
+    static void Display(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+}
