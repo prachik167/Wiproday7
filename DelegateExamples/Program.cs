be0a77f792fb37a03795fc18fd2a84f73baa009c
prachik167 <128143076+prachik167@users.noreply.github.com>
2025-02-04 14:03:36 +0530

    Add files via upload

---
+++ DelegateExamples/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DelegateExamples
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student() {Id = 1, Name ="ABC", Marks = 80 },
+                new Student() {Id = 2, Name ="DEF", Marks =70 },
+                new Student() {Id = 3, Name = "GHI", Marks =90 }
+            };
+
+            StudentDelegate studentDelegate = new StudentDelegate(IsEligible);
+            Student.PromoteStudents(students, studentDelegate);
+
+        }
+
+        static bool IsEligible(Student student)
+        {
+            return student.Marks > 75;
+        }
+    }
+}
---
+++ DelegateExamples/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExamples
+{
+    public delegate bool StudentDelegate(Student s);
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Marks { get; set; }
+
+        public static void PromoteStudents(List<Student> students, StudentDelegate isPromotable)
+        {
+            foreach (var student in students)
+            {
+                if (isPromotable(student))
+                {
+                    Console.WriteLine(student.Name + "is Promoted");
+                }
+
+            }
+
+        }
+    }
+}
